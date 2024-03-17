9ad4c641f7aaf695e5a8f545606a230d393f2cd5
gornikprzemyslaw <gornik.przemek@gmail.com>
2024-03-17 11:27:03 +0200

    test pipeline

--- aks-version/aksversion.py
+++ aks-version/aksversion.py
@@ -19,9 +19,6 @@ class Email(object):
     second_recipient_address = "gornik.przemek@gmail.com"
 
     def send_email_to_multiple_recipients(self):
-        # creating the email client
-        email_client = EmailClient.from_connection_string(self.connection_string)
-
 
         def check_last_aks_versions():
             client = ContainerServiceClient(
@@ -43,96 +40,94 @@ class Email(object):
 
             print(f"List of AKS versions: {list_of_versions}")
             print(f"List of AKS preview: {list_of_preview_versions}")
-            return max(list_of_versions), max(list_of_preview_versions)
 
+            last_version = max(list_of_versions)
+            if len(list_of_preview_versions) == 0:
+                preview_version = null
+            else:
+                preview_version = max(list_of_preview_versions)
+            return last_version, preview_version
 
         last_version, preview_version = check_last_aks_versions()
         print(f"Last AKS version: {last_version}")
         print(f"Preview version: {preview_version}")
 
 
+        def read_blob():
+            from azure.storage.blob import BlobClient, BlobServiceClient
+            import json
+            import ast
 
-        # creating the email message
-        message = {
-            "content": {
-                "subject": "This is the subject",
-                "plainText": "This is the body",
-                "html": f"<html><h1>Last AKS version is: {last_version}</h1></html>"
-            },
-            "recipients": {
-                "to": [
-                    {"address": self.recipient_address, "displayName": "Customer Name"},
-                    {"address": self.second_recipient_address, "displayName": "Customer Name 2"}
-                ],
-                "cc": [
-                    {"address": self.recipient_address, "displayName": "Customer Name"},
-                    {"address": self.second_recipient_address, "displayName": "Customer Name 2"}
-                ],
-                "bcc": [
-                    {"address": self.recipient_address, "displayName": "Customer Name"},
-                    {"address": self.second_recipient_address, "displayName": "Customer Name 2"}
-                ]
-            },
-            "senderAddress": self.sender_address
-        }
+            account_url = "https://aksversion.blob.core.windows.net/"
 
-        try:
-            # sending the email message
-            poller = email_client.begin_send(message)
-            response = poller.result()
-            print("Operation ID: " + response['id'])
-        except HttpResponseError as ex:
-            print(ex)
-            pass
+            creds = DefaultAzureCredential()
+            service_client = BlobServiceClient(
+                account_url=account_url,
+                credential=creds
+            )
+
+            blob_name = "aks_versions.json"
+            container_name = "versions"
+            blob_url = f"{account_url}/{container_name}/{blob_name}"
+
+            blob_client = BlobClient.from_blob_url(
+                blob_url=blob_url,
+                credential=creds
+            )
+            # read blob
+            blob_download = blob_client.download_blob()
+            blob_content = blob_download.readall().decode("utf-8")
+
+            saved_aks_versions = ast.literal_eval(blob_content)
+            # print( saved_aks_versions[0])
+            # print(type( saved_aks_versions))
+            return saved_aks_versions[0]
+
+
+
+        saved_version = read_blob()
+        print(f"Saved_version: {saved_version}")
+
+
+        def send_message():
+
+            # creating the email client
+            email_client = EmailClient.from_connection_string(self.connection_string)
+            # creating the email message
+            message = {
+                "content": {
+                    "subject": "Last AKS version",
+                    "plainText": "This is the body",
+                    "html": f"<html><h1>Last AKS version is: {last_version}</h1></html>"
+                },
+                "recipients": {
+                    "to": [
+                        {"address": self.recipient_address, "displayName": "Customer Name"},
+                        {"address": self.second_recipient_address, "displayName": "Customer Name 2"}
+                    ],
+                    "cc": [
+                        {"address": self.recipient_address, "displayName": "Customer Name"},
+                        {"address": self.second_recipient_address, "displayName": "Customer Name 2"}
+                    ],
+                    "bcc": [
+                        {"address": self.recipient_address, "displayName": "Customer Name"},
+                        {"address": self.second_recipient_address, "displayName": "Customer Name 2"}
+                    ]
+                },
+                "senderAddress": self.sender_address
+            }
+
+            try:
+                # sending the email message
+                poller = email_client.begin_send(message)
+                response = poller.result()
+                print("Operation ID: " + response['id'])
+            except HttpResponseError as ex:
+                print(ex)
+                pass
+
+        send_message()
 
 if __name__ == '__main__':
     sample = Email()
     sample.send_email_to_multiple_recipients()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#connection_string = "endpoint=https://akstest.unitedstates.communication.azure.com/;accesskey=Ct2cXmnwnYKW8eGEiA0BVdjIJcXJM+twFBox/SF9fTGf1kORB1zUMwj/WT7Xdf4QL28KCHt8RivTGjkrBynbLQ=="
-#client = EmailClient.from_connection_string(connection_string);
-
-"""
-# PREREQUISITES
-    pip install azure-identity
-    pip install azure-mgmt-containerservice
-# USAGE
-    python kubernetes_versions_list.py
-
-    Before run the sample, please set the values of the client ID, tenant ID and client secret
-    of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
-    AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
-    https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
-"""
-
-
-# def main():
-#     client = ContainerServiceClient(
-#         credential=DefaultAzureCredential(),
-#         subscription_id="9c68d842-8240-43e8-9cad-3495f9769729",
-#     )
-#
-#     response = client.managed_clusters.list_kubernetes_versions(
-#         location="westeurope",
-#     )
-#     print(response.values)
-    # for i in response.values:
-    #     print(i)
-
-
-# # x-ms-original-file: specification/containerservice/resource-manager/Microsoft.ContainerService/aks/stable/2023-11-01/examples/KubernetesVersions_List.json
-# if __name__ == "__main__":
-#     main()
